c6e46b38c8d56d5719b1d6af5f8d0629a1b0b004
unknown <mohab.ali01@eng-st.cu.edu.eg>
2023-03-17 18:14:19 +0200

    ui base

---
+++ mainwindow.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "opencv2/world.hpp"
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QImage>
+#include <QVector>
+#include <QQueue>
+#include <QDebug>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
+QString imgPath;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionUpload_triggered()
+{
+    imgPath = QFileDialog::getOpenFileName(this, "Open an Image", "..", "Images (*.png *.xpm *.jpg *.bmb)");
+
+    // read image using opencv
+    if(imgPath.isEmpty())
+        return;
+
+    // tab 1
+
+    Mat image = imread(imgPath.toStdString());
+    cvtColor(image, image,COLOR_BGR2RGB);
+    showImg(image, ui->originalImg, QImage::Format_RGB888, this->origWidth, this->origHeight);
+
+
+}
+void MainWindow::showImg(Mat& img, QLabel* imgLbl, enum QImage::Format imgFormat, int width , int hieght)
+{
+    QImage image2((uchar*)img.data, img.cols, img.rows, imgFormat);
+    QPixmap pix = QPixmap::fromImage(image2);
+    int width_img = ui->originalImg->width();
+    int height_img = ui->originalImg->height();
+    imgLbl->setPixmap(pix.scaled(width_img,height_img,Qt::KeepAspectRatio));
+}
---
+++ mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "opencv2/world.hpp"
+
+#include <QMainWindow>
+#include <QLabel>
+using namespace cv;
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    void showImg(Mat& img, QLabel* imgLbl, enum QImage::Format imgormat, int width, int hieght);
+    int origWidth, origHeight;
+
+    ~MainWindow();
+private slots:
+
+    void on_actionUpload_triggered();
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
